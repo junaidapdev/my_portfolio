1b46f57026e16b05ebc2cb977f5b6b6bc1776ed1
junaidapdev <junaidap.dev.com>
2022-06-05 17:14:58 +0530

    components added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Blogs from '../Blogs/Blogs'
+import Contact from '../Contact/Contact'
+import Home from '../Home/Home'
+import Products from '../Products/Products'
+import Projects from '../Projects/Projects'
+import Skills from '../Skills/Skills'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return ( 
+      <BrowserRouter>
+  <div>
+    <Route exact path='/' component={Home}/>
+    <Route path='/skills' component={Skills}/>
+    <Route path='/projects' component={Projects}/>
+    <Route path='/products' component={Products}/>
+    <Route path='/blogs' component={Blogs}/>
+    <Route path='/contact' component={Contact }/>
+  </div>
+      </BrowserRouter>
+    )
 }
 
 export default App
